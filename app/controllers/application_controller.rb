d224029762befb34b792d146cec6fb797df24b93
Stephen Kusrianto <stephenkusrianto@gmail.com>
2021-12-07 14:32:01 +0800

    login with wallet id now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:wallet_id])
-    # devise_parameter_sanitizer.permit(:sign_in, keys: [:wallet_id])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:wallet_id])
   end
 end
