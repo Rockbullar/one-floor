cf56dd8903a86cc8b8f6dedde4dd12871f8410a6
Soh Zhen Yi <sohzhenyi@gmail.com>
2021-12-07 18:31:38 +0800

    seed is completeeeeeeeeeeeeeee

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,7 @@ puts "Cleanup DB"
 Nft.destroy_all
 Collection.destroy_all
 
-contract_arr = ["0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"]
-# contract_arr = ["0x2bd60f290060451e3644a7559d520c2e9b32c7e9","0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d","0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb","0x50f5474724e0ee42d9a4e711ccfb275809fd6d4a","0x60e4d786628fea6478f785a6d7e704777c86a7c6","0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d","0x999e88075692bcee3dbc07e7e64cd32f39a1d3ab","0x469823c7b84264d1bafbcd6010e9cdf1cac305a3","0x9bf252f97891b907f002f2887eff9246e3054080","0xd4e4078ca3495de5b1d4db434bebc5a986197782"]
+contract_arr = ["0x2bd60f290060451e3644a7559d520c2e9b32c7e9","0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d","0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb","0x50f5474724e0ee42d9a4e711ccfb275809fd6d4a","0x60e4d786628fea6478f785a6d7e704777c86a7c6","0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d","0x999e88075692bcee3dbc07e7e64cd32f39a1d3ab","0x469823c7b84264d1bafbcd6010e9cdf1cac305a3","0x9bf252f97891b907f002f2887eff9246e3054080","0xd4e4078ca3495de5b1d4db434bebc5a986197782"]
 
 def create_collection(collection, contract)
   project = Collection.new({
@@ -36,6 +35,7 @@ def create_collection(collection, contract)
   project.save!
   puts "collection #{project.name} created"
   create_nft(project, project.contract_id)
+  puts "All collections creation completed"
 end
 
 def create_nft(collection, contract_id)
@@ -50,7 +50,7 @@ def create_nft(collection, contract_id)
 
   # API is set to fetch 10 assets already, can amend as required
   nft_array.each do |nft|
-    nft = Nft.new ({
+    nft = Nft.new({
       token_id: nft["token_id"],
       contract_id: contract_id,
       last_sale_eth_price: nft["last_sale"].nil? ? 0 : nft["last_sale"]["total_price"],
@@ -66,7 +66,16 @@ def create_nft(collection, contract_id)
     bid_response = bid_http.request(bid_request)
     bid_parsed = JSON.parse(bid_response.read_body)
 
-    nft.highest_bid_eth_price = bid_parsed["orders"].empty? ? 0 : bid_parsed["orders"][0]["current_price"].to_f #to update the itiration to compare largest bid later
+    if bid_parsed["orders"].empty?
+      nft.highest_bid_eth_price = 0
+    else
+      order_arr = bid_parsed["orders"]
+      highest_bid = order_arr.max_by do |bid|
+        bid["current_price"]
+      end
+
+      nft.highest_bid_eth_price = highest_bid["current_price"].to_f
+    end
     nft.save!
   end
   puts "All NFTs creatd for the collection"
@@ -81,5 +90,4 @@ contract_arr.each do |contract|
   collection = JSON.parse(response.read_body)
 
   create_collection(collection, contract)
-  puts "All collections creation completed"
 end
