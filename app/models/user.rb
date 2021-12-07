c0c8d59a0aebedc04c0090389e9b3bd395a1b6bd
Benedict Ng <benedictngbh@gmail.com>
2021-12-07 13:33:40 +0800

    favoritors added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :nfts, through: :portfolio
   has_many :collections, through: :nfts
+  
+  acts_as_favoritor
 end
