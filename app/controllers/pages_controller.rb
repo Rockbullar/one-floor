bac049acb2be498f2851d7ea55ac164dae387d14
Benedict Ng <benedictngbh@gmail.com>
2021-12-07 11:46:26 +0800

    rescue api error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @test = 'xxx'
     etherscan_key = ENV["ETHERSCAN_KEY"]
-    @eth_usd = JSON.parse(URI.open("https://api.etherscan.io/api?module=stats&action=ethprice&apikey=#{etherscan_key}").read)["result"]["ethusd"]
-    @gas_price = JSON.parse(URI.open("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=#{etherscan_key}").read)["result"]["SafeGasPrice"]
+    begin
+      @eth_usd = JSON.parse(URI.open("https://api.etherscan.io/api?module=stats&action=ethprice&apikey=#{etherscan_key}").read)["result"]["ethusd"]
+    rescue
+      @eth_usd = 'error'
+    end
+
+    begin
+      @gas_price = JSON.parse(URI.open("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=#{etherscan_key}").read)["result"]["SafeGasPrice"]
+    rescue
+      @gas_price = 'error'
+    end
   end
 end
