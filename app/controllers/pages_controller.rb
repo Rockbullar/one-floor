f7732622fd08ead29400027896d221a3fe7df80a
Stephen Kusrianto <stephenkusrianto@gmail.com>
2021-12-08 16:43:50 +0800

    completed API call

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,24 +15,30 @@ class PagesController < ApplicationController
     rescue
       @gas_price = 'error'
     end
+
+    begin
+      @portfolio = portfolio
+    rescue
+      @portfolio = 'xxx'
+    end
   end
 
   def about
-
   end
 
-  def portfolio
-    wallet = current_user.wallet_id
-    nfts = []
+  private
 
-    port_url = URI("https://api.opensea.io/api/v1/assets?owner=#{wallet}&order_direction=desc&offset=0&limit=20")
+  def portfolio
+    @wallet = current_user.wallet_id
+    @nfts = []
+
+    port_url = URI("https://api.opensea.io/api/v1/assets?owner=#{@wallet}&order_direction=desc&offset=0&limit=20")
     port_http = Net::HTTP.new(port_url.host, port_url.port)
     port_http.use_ssl = true
     port_request = Net::HTTP::Get.new(port_url)
     port_response = port_http.request(port_request)
     port_parsed = JSON.parse(port_response.read_body)
-    nft_array = nft_parsed["assets"]
-
+    nft_array = port_parsed["assets"]
     # iteration for each nfts in portfolio
     nft_array.map do |nft|
       nft = Nft.new({
@@ -55,28 +61,27 @@ class PagesController < ApplicationController
       bid_parsed = JSON.parse(bid_response.read_body)
 
       if bid_parsed["orders"].empty?
-        nft.highest_bid_eth_price = 0
+        nft["highest_bid_eth_price"] = 0
       else
         order_arr = bid_parsed["orders"]
         highest_bid = order_arr.max_by do |bid|
           bid["current_price"]
         end
 
-        nft.highest_bid_eth_price = highest_bid["current_price"].to_f
+        nft["highest_bid_eth_price"] = highest_bid["current_price"].to_f
       end
 
-      project = Collection.new({
-        name: collection["collection"]["name"],
-        description: collection["collection"]["description"],
-        slug: collection["collection"]["slug"],
-        twitter_username: collection["collection"]["twitter_username"],
-        image_url: collection["collection"]["featured_image_url"],
-        discord_url: collection["collection"]["discord_url"],
-        contract_id: contract
-      })
-      nft.collection = collection
-      nfts << nft
+      stats_url = URI("https://api.opensea.io/api/v1/collection/#{nft.slug}/stats")
+      stats_http = Net::HTTP.new(stats_url.host, stats_url.port)
+      stats_http.use_ssl = true
+      stats_request = Net::HTTP::Get.new(stats_url)
+      stats_response = stats_http.request(stats_request)
+      stats_collection = JSON.parse(stats_response.read_body)
+
+      # To remove later as total supply should be part of API call
+      nft["floor_price"] = stats_collection["stats"]["floor_price"].to_f
+      @nfts << nft
     end
-    return nfts
+    return @nfts
   end
 end
