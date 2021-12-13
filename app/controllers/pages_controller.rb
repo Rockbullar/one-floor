d8f20c675d876f8173670150c77fc7b2ca2cc217
Stephen Kusrianto <stephenkusrianto@gmail.com>
2021-12-13 10:53:51 +0800

    cleaned out changes for eth and gas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,19 +7,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    # etherscan_key = ENV["ETHERSCAN_KEY"]
-    # begin
-    #   @eth_usd = JSON.parse(URI.open("https://api.etherscan.io/api?module=stats&action=ethprice&apikey=#{etherscan_key}").read)["result"]["ethusd"]
-    # rescue
-    #   @eth_usd = 'error'
-    # end
 
     begin
-      # @gas_price = JSON.parse(URI.open("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=#{etherscan_key}").read)["result"]["SafeGasPrice"]
       @gas_seller = opengasscraper[4][0].to_f
       @gas_buyer = opengasscraper[5][0].to_f
     rescue
-      # @gas_price = 'error'
       @gas_seller = 'error'
       @gas_buyer = 'error'
     end
@@ -36,30 +28,6 @@ class PagesController < ApplicationController
 
     articles_service = Articles.new
     @articles = articles_service.call
-
-    # @nfts = Nft.all
-    # @nfts = User.where(wallet_id: "0x241Af3d9a9959d0E78353Ff26f62A3eB7798202D").nfts
-
-    # begin
-    #   @nfts = current_user.nfts
-    #   raise
-
-    #   #   # stats_url = URI("https://api.opensea.io/api/v1/collection/#{nft.slug}/stats")
-    #   #   # stats_http = Net::HTTP.new(stats_url.host, stats_url.port)
-    #   #   # stats_http.use_ssl = true
-    #   #   # stats_request = Net::HTTP::Get.new(stats_url)
-    #   #   # stats_response = stats_http.request(stats_request)
-    #   #   # stats_collection = JSON.parse(stats_response.read_body)
-
-    #   #   # # To remove later as total supply should be part of API call
-    #   #   # nft["floor_price"] = stats_collection["stats"]["floor_price"].to_f
-    #   #   # @nfts << nft
-    #   # end
-    #   # @portfolio = @nfts
-    # rescue
-    #   raise
-    #   @portfolio = 'xxx'
-    # end
   end
 
   private
