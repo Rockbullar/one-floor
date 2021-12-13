d3d49baebae301d4a48490ccda480639f4df84b1
Geetha Bheema <geetha.bheema01@gmail.com>
2021-12-13 23:13:33 +0800

    carousell pending but landing page gooooood

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'about', to: 'pages#about'
-<<<<<<< HEAD
   get 'watchlist', to: 'pages#watchlist'
-=======
   get 'landing', to: 'pages#landing'
->>>>>>> e503c87 (marquee-less landing page)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
