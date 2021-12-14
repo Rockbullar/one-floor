3200b89d6c8b0e765a6fa5a9e5af5c929e7b9335
Benedict Ng <benedictngbh@gmail.com>
2021-12-14 18:03:30 +0800

    save

--- app/javascript/plugins/init_add_to_watchlist.js
+++ app/javascript/plugins/init_add_to_watchlist.js
@@ -1,5 +1,8 @@
 const addCollectionButton = document.querySelector('#add-collection-to-watchlist');
 const addCollectionForm = document.querySelector('#watchlist-collection-form');
+const addCollectionInput = document.querySelector('#add-collection-input');
+
+} while (condition);
 
 const showAddWatchlistCollectionForm = () => {
   if (addCollectionButton) {
@@ -10,5 +13,28 @@ const showAddWatchlistCollectionForm = () => {
   }
 }
 
+list = document.querySelector('search-results')
+const displayCollections = () => {
+  if (addCollectionInput) {
+    addCollectionInput.addEventListener('click', (event) => {
+      event.preventDefault();
+      list.innerHTML = '';
+      const input = document.querySelector('#search-input');
+      fetchMovies(input.value);
+    });
+  }
+}
 
-export { showAddWatchlistCollectionForm }
+const insertMovies = (data) => {
+  data.Search.forEach((result) => {
+    const movie = `<li>
+      <img src="${result.Poster}" alt="" />
+      <p>${result.Title}</p>
+    </li>`;
+    list.insertAdjacentHTML('beforeend', movie);
+  });
+};
+
+
+
+export { showAddWatchlistCollectionForm, displayCollections }
