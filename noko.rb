6dec12a94fcd389a09ce453d2ecde79b9c9f44bd
Stephen Kusrianto <stephenkusrianto@gmail.com>
2021-12-14 14:21:32 +0800

    completed migration and api call for collection model

--- noko.rb
+++ noko.rb
@@ -2,12 +2,9 @@ require 'open-uri'
 require 'nokogiri'
 
   def listedcountscraper
-    html_content = URI.open('https://opensea.io/collection/boredapeyachtclub?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW').read
+    html_content = URI.open("https://opensea.io/collection/sandbox?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW").read
     doc = Nokogiri::HTML(html_content)
-    doc.search('div.AssetSearchView--results-count').map do |count|
-      result = count.text.strip
-      result
-    end
+    result = doc.search('div.AssetSearchView--results-count').first.text.strip.gsub(/(\,?\D*)/,'').to_f
   end
 
-  p listedcountscraper
+ p listedcountscraper
