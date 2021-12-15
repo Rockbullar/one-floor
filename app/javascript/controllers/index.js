fc615bacf3abf492a14689611bd0557b497288d7
Soh Zhen Yi <sohzhenyi@gmail.com>
2021-12-15 18:11:38 +0800

    tool tip wip

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,3 +7,22 @@
 // const application = Application.start()
 // const context = require.context("controllers", true, /_controller\.js$/)
 // application.load(definitionsFromContext(context))
+
+// Tooltip
+import { createPopper } from '@popperjs/core';
+import './styles.css';
+
+const popcorn = document.querySelector('#popcorn');
+const tooltip = document.querySelector('#tooltip');
+
+createPopper(popcorn, tooltip, {
+  placement: 'top',
+  modifiers: [
+    {
+      name: 'offset',
+      options: {
+        offset: [0, 8],
+      },
+    },
+  ],
+});
